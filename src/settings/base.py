537cd5467f46cc5beadbf06c13ca1debd12918b4
sanjaygd <sanjaygd96@gmail.com>
2023-10-12 19:55:47 +0530

    commented prints

--- src/settings/base.py
+++ src/settings/base.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY", default="django-insecure$simple.settings.local")
 
-print('SECRET_KEY',SECRET_KEY)
+# print('SECRET_KEY',SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
